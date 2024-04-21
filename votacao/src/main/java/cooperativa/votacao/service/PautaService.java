7e2f04ececf28f5a6a3dacd64abb3ed6081e55f7
guibg <gui.bgoulart@gmail.com>
2024-04-21 20:06:34 -0300

    adiciona logica inicial criar pauta

--- votacao/src/main/java/cooperativa/votacao/service/PautaService.java
+++ votacao/src/main/java/cooperativa/votacao/service/PautaService.java
@@ -1,11 +1,17 @@
 package cooperativa.votacao.service;
 
 import cooperativa.votacao.entity.Pauta;
+import cooperativa.votacao.repository.PautaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PautaService {
+
+    @Autowired
+    private PautaRepository pautaRepository;
+
     public Pauta createPauta(Pauta pauta) {
-        return null;
+        return pautaRepository.save(pauta);
     }
 }
