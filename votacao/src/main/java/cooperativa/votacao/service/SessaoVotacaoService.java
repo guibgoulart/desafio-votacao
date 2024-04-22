d9211a25735959cf8ae10b07912c8243df818c25
guibg <gui.bgoulart@gmail.com>
2024-04-22 02:20:54 -0300

    adiciona tratamento de exceçao e logging para criar votacao

--- votacao/src/main/java/cooperativa/votacao/service/SessaoVotacaoService.java
+++ votacao/src/main/java/cooperativa/votacao/service/SessaoVotacaoService.java
@@ -4,12 +4,14 @@ import cooperativa.votacao.entity.Pauta;
 import cooperativa.votacao.entity.SessaoVotacao;
 import cooperativa.votacao.repository.PautaRepository;
 import cooperativa.votacao.repository.SessaoVotacaoRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 
 @Service
+@Log4j2
 public class SessaoVotacaoService {
 
     @Autowired
@@ -19,7 +21,25 @@ public class SessaoVotacaoService {
     PautaService pautaService;
 
     public SessaoVotacao abrirSessao(long pautaId, Duration duracao) {
-        Pauta pauta = pautaService.getPautaById(pautaId);
-        return sessaoVotacaoRepository.save(new SessaoVotacao(pauta, duracao));
+        try {
+            log.trace("Abrindo sessão de votação para pauta: {}", pautaId);
+            Pauta pauta = pautaService.getPautaById(pautaId);
+            SessaoVotacao sessaoVotacao = new SessaoVotacao(pauta, duracao);
+
+            SessaoVotacao savedSessaoVotacao = sessaoVotacaoRepository.save(sessaoVotacao);
+
+            // verificar se teve sucesso e logar informaçoes
+            if (savedSessaoVotacao == null) {
+                log.error("Falha ao salvar a sessão de votação para a pauta: {}", pautaId);
+                throw new RuntimeException("Falha ao salvar a sessão de votação");
+            } else {
+                log.info("Sessão de votação {} foi aberta com sucesso", savedSessaoVotacao);
+            }
+
+            return savedSessaoVotacao;
+        } catch (Exception e) {
+            log.error("Erro ao abrir sessão de votação: {}", e.getMessage());
+            throw new RuntimeException("Erro ao abrir sessão de votação", e);
+        }
     }
 }
